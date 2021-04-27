9d46e40a592a6514a1e5407a59f5a77192cf4f65
Ali Alpay <alialpay96@hotmail.com>
2021-04-27 23:06:56 +0300

    homework 2.2

---
+++ homework2/src/methods/Main.java
@@ -0,0 +1,38 @@
+package methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		sayiBulmaca();
+		sayiBulmaca();
+		sayiBulmaca();
+		sayiBulmaca();
+	}
+
+	public static void sayiBulmaca() {
+		int[] sayilar = { 1, 2, 5, 7, 9, 0 };
+
+		boolean varMi = false;
+		int aranacak = 3;
+
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		
+		String mesaj ="";
+		if (varMi == true) {
+			mesaj = "Sayý mevcuttur: "+aranacak;
+			mesajVer(mesaj);
+		} else {
+			mesajVer("Sayý mevcut deðildir: "+aranacak);
+		}
+	}
+	
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+	}
+
+}
