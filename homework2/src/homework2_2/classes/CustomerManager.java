56db9d4634d5c1b79c4b661071464443437a8002
Ali Alpay <alialpay96@hotmail.com>
2021-04-28 00:05:00 +0300

    homework 2.2

---
+++ homework2/src/homework2_2/classes/CustomerManager.java
@@ -0,0 +1,13 @@
+package homework2_2.classes;
+
+public class CustomerManager {
+	public void Add() {
+		System.out.println("Müþteri eklendi");
+	}
+	public void Remove() {
+		System.out.println("Müþteri silindi");
+	}
+	public void Update() {
+		System.out.println("Müþteri güncellendi");
+	}
+}
