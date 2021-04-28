39e4855d44252675b9e2d160dd0ba6126dd9d8ce
Ali Alpay <alialpay96@hotmail.com>
2021-04-28 22:51:09 +0300

    3. day commit1

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,47 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Product product1 = new Product(1,"Lenovo V14", 1500, "16 gb ram");	
+		Product product1 = new Product(1,"Lenovo V14", 1500, "16 gb ram",10);	
 		
 		
 		Product product2 = new Product();
-		product2.id=2;
-		product2.name="Lenovo V15";
-		product2.unitPrice=16000;
-		product2.detail="32 gb ram";
+		product2.setId(2);
+		product2.setName("Lenovo V15");
+		product2.setDetail("16 gb ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(16000);
 		
-		Product product3 = new Product();
-		product3.id=3;
-		product3.name="Hp 5";
-		product3.unitPrice=10000;
-		product3.detail="8 gb ram";
-		
-		
-		Product[] products = {product1, product2, product3};
-		
-		for(Product product : products) {
-			System.out.println(product.name);
-		}
-		
-		System.out.println(products.length);
-		
-		
+		System.out.println(product2.getUnitPriceAfterDiscount());
 		
 		Category category1 = new Category();
-		category1.id=1;
-		category1.name="Bilgisayar";
+		category1.setId(1);
+		category1.setName("Ýçecek");
 		
 		Category category2 = new Category();
-		category2.id=2;
-		category2.name="Ev/Bahçe";
+		category2.setId(1);
+		category2.setName("Yiyecek");
 		
-		
-		ProductManager productManager = new ProductManager();
-		productManager.addToCart(product1);
-		
-		productManager.addToCart(product2);
-		
-		productManager.addToCart(product3);
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
 
 	}
 
